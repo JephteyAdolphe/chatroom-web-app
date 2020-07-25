2d396556525b2c79ff3546ef588d405b8759260d
JephteyAdolphe <ja983@scarletmail.rutgers.edu>
2020-07-25 19:19:56 -0400

    Progress

--- Backend/README.md
+++
@@ -1,2 +0,0 @@
-# chatroom-web-app
-Chat app built on Python's Django framework with a React frontend
--- client/src/App.js
+++ client/src/App.js
@@ -2,9 +2,8 @@ import React from 'react';
 
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
-import Join from './components/Join';
-import Chat from './components/Chat';
-
+import Join from './components/Join/Join';
+import Chat from './components/Chat/Chat';
 
 const App = () => (
     <Router>
@@ -12,6 +11,5 @@ const App = () => (
         <Route path="/chat" component={Chat} />
     </Router>
 );
-
 
 export default App;
